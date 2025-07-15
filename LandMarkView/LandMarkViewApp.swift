4c1e33e5184de94afb2b7434489f70d03279dfc2
dhruvSuhird <suhiriddhruv218@gmail.com>
2025-07-15 18:54:55 +0530

    Initial Commit

---
+++ LandMarkView/LandMarkViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkViewApp.swift
+//  LandMarkView
+//
+//  Created by Dhruv Suhird on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
